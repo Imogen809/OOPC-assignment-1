0b333bf4945289eefca2708b300f4040dc81198d
Imogen809 <imogen.strong@student.manchester.ac.uk>
2026-02-10 12:11:24 +0000

    Update Assignment-1.cpp

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -5,18 +5,38 @@
 #include<iostream>
 #include<iomanip>
 
+using namespace std;
+
 int main()
 {
   // Declare variables here
+  const double rydberg_energy = 13.6; // The rydberg energy in eV.
 
   // Ask user to enter atomic number
+  cout << "Please enter an atomic number:";
+  cin >> Z;
 
   // Ask user to enter initial and final quantum numbers
+  cout << "Please enter the initial quantum number:";
+  cin >> n_i;
 
-  // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_j^2-1/n_i^2) eV
+  cout << "Please enter the final quantum number:";
+  cin >> n_f;
 
+  // Check that final quantum number < initial qunatum number
+  if (final_quantum_number >= initial_quantum_number) {
+        cout << "Error: final quantum number must be less than initial quantum number." << endl;
+        return 1;  // exit with error
+    }
+
+  cout << "Inputs accepted." << endl;
+  
+
+  // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_f^2-1/n_i^2) eV
+  double E = rydberg_energy * (1.0/(Z*Z)) * (1.0/(n_f*n_f) - 1.0/(n_i*n_i)) 
+  
   // Output answer
-  std::cout << "Here is my answer:" << "..." << std::endl;
+  cout << "Here is my answer:" << E << "eV" << endl;
 
   return 0;
 }
