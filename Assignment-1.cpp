34078aa66018a27368c57b8a3b04e02a96165d15
Imogen809 <imogen.strong@student.manchester.ac.uk>
2026-02-10 12:53:09 +0000

    Update Assignment-1.cpp

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -11,32 +11,48 @@ int main()
 {
   // Declare variables here
   const double rydberg_energy = 13.6; // The rydberg energy in eV.
+  int Z; // Atomic number
+  int n_i; // Initial quantum number
+  int n_f; // Final quantum number
+  string repeat; // Answer to question to repeat
 
-  // Ask user to enter atomic number
-  cout << "Please enter an atomic number:";
-  cin >> Z;
-
-  // Ask user to enter initial and final quantum numbers
-  cout << "Please enter the initial quantum number:";
-  cin >> n_i;
-
-  cout << "Please enter the final quantum number:";
-  cin >> n_f;
-
-  // Check that final quantum number < initial qunatum number
-  if (final_quantum_number >= initial_quantum_number) {
-        cout << "Error: final quantum number must be less than initial quantum number." << endl;
-        return 1;  // exit with error
+  do{
+    while (true){
+      // Ask user to enter atomic number
+      cout << "Please enter an atomic number:";
+      cin >> Z;
+    
+      // Ask user to enter initial and final quantum numbers
+      cout << "Please enter the initial quantum number:";
+      cin >> n_i;
+    
+      cout << "Please enter the final quantum number:";
+      cin >> n_f;
+    
+      // Check that final quantum number < initial qunatum number
+      if (n_f <= n_i) {
+          cout << "Inputs accepted." << endl;
+          break; // exit the loop if inputs are valid
+        }
+    
+      else{
+          cout << "Error: final quantum number must be less than initial quantum number." << endl;
+          cout << "Please try again." << endl;
+  
+      }
+  
     }
-
-  cout << "Inputs accepted." << endl;
   
-
-  // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_f^2-1/n_i^2) eV
-  double E = rydberg_energy * (1.0/(Z*Z)) * (1.0/(n_f*n_f) - 1.0/(n_i*n_i)) 
+    // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_f^2-1/n_i^2) eV
+    double E = rydberg_energy * (1.0/(Z*Z)) * (1.0/(n_f*n_f) - 1.0/(n_i*n_i));
+    
+    // Output answer
+    cout << "Here is my answer:" << E << "eV" << endl;
   
-  // Output answer
-  cout << "Here is my answer:" << E << "eV" << endl;
-
+    // Ask if user would like to repeat
+    cout << "Would you like to repeat this calculation? (Y/N):";
+    cin >> repeat;
+    } while (repeat == "Y" || repeat == "y"); // repeat if user enters Y or y
+  
   return 0;
 }
